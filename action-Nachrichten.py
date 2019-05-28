b454d375c053eee01e57f4bb5f52cc35aa40ce01
hablijack <christoph.habel@posteo.de>
2019-05-28 15:14:46 +0200

    implemented podcast download of tagesschau.de

---
+++ action-Nachrichten.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import configparser
+from hermes_python.hermes import Hermes, MqttOptions
+import io
+import toml
+import re
+import feedparser
+from subprocess import Popen
+
+USERNAME_INTENTS = "hablijack"
+MQTT_BROKER_ADDRESS = "localhost:1883"
+MQTT_USERNAME = None
+MQTT_PASSWORD = None
+
+def add_postfix(intent_name):
+    return USERNAME_INTENTS + ":" + intent_name
+
+def intent_callback_news(hermes, intent_message):
+    tagesschau_url = 'http://www.tagesschau.de/export/podcast/hi/tagesschau-in-100-sekunden/'
+    feed = feedparser.parse(tagesschau_url)
+    podcast_url = feed['entries'][0]['links'][0]['href']
+    player = Popen(["mpg123", podcast_url])
+    hermes.publish_end_session(intent_message.session_id, "")
+
+if __name__ == "__main__":
+    snips_config = toml.load('/etc/snips.toml')
+    if 'mqtt' in snips_config['snips-common'].keys():
+        MQTT_BROKER_ADDRESS = snips_config['snips-common']['mqtt']
+    if 'mqtt_username' in snips_config['snips-common'].keys():
+        MQTT_USERNAME = snips_config['snips-common']['mqtt_username']
+    if 'mqtt_password' in snips_config['snips-common'].keys():
+        MQTT_PASSWORD = snips_config['snips-common']['mqtt_password']
+    mqtt_opts = MqttOptions(username=MQTT_USERNAME, password=MQTT_PASSWORD, broker_address=MQTT_BROKER_ADDRESS)
+
+    with Hermes(mqtt_options=mqtt_opts) as h:
+        h.subscribe_intent(add_postfix("newsInfo"), intent_callback_fuel)
+        h.start()
---
+++ setup.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash -e
+
+# Copy config.ini.default if config.ini doesn't exist.
+if [ ! -e config.ini ]
+then
+    cp config.ini.default config.ini
+fi
+
+PYTHON=`which python3`
+VENV=venv
+
+if [ -f "$PYTHON" ]
+then
+
+    if [ ! -d $VENV ]
+    then
+        # Create a virtual environment if it doesn't exist.
+        $PYTHON -m venv $VENV
+    else
+        if [ -e $VENV/bin/python2 ]
+        then
+            # If a Python2 environment exists, delete it first
+            # before creating a new Python 3 virtual environment.
+            rm -r $VENV
+            $PYTHON -m venv $VENV
+        fi
+    fi
+
+    # Activate the virtual environment and install requirements.
+    . $VENV/bin/activate
+    pip3 install -r requirements.txt
+
+else
+    >&2 echo "Cannot find Python 3. Please install it."
+fi
